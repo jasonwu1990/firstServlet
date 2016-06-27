56537403543d3df39808d53789a1238d6f97765d
jasonwu1990 <jasonwu1990@163.com>
2016-06-27 16:26:13 +0800

    first commit

---
+++ src/com/jason/test/TestServlet.java
@@ -0,0 +1,51 @@
+package com.jason.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw =  response.getWriter();
+		pw.write("<!doctype html>");
+		
+		pw.write("<html>");
+		pw.write("<head>");
+		pw.write("<title>");
+		pw.write("servltTest");
+		pw.write("</title>");
+		pw.write("</head>");
+		pw.write("<body>");
+		pw.write("firstServlet");
+		pw.write("</body>");
+		pw.write("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
